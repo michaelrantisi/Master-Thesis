1a4b1b73bdecd24c126189e82fd191e52889e9b6
Michael rantisi <134786856+michaelrantisi@users.noreply.github.com>
2026-05-03 19:49:06 +0300

    Delete 0_check_gpu.py

--- 0_check_gpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-print("Torch:", torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA device count:", torch.cuda.device_count())
-
-if torch.cuda.is_available():
-    print("Device:", torch.cuda.get_device_name(0))
-    print("Capability:", torch.cuda.get_device_capability(0))
